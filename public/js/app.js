885ee7893b78714046fa16a60e9831af364190ee
Vasile Gorcinschi <vasigorc@gmail.com>
2020-03-30 21:15:31 -0400

    Added fetch to FE app

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
 console.log('Client side javascript file is loaded')
+
+
+// returns the first element that matches the query
+const weatherForm = document.querySelector('form')
+
+weatherForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const search = document.querySelector('input')
+    const location = search.value
+    fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+        response.json().then(data => console.log(data)).catch(error => console.log(error))
+    })
+
+})
